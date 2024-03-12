3b6113daeb5f49298c2ec6c616e8db7ae55baa96
Rybalovms <Rybalovms@310D-10>
2024-03-12 14:36:53 +0700

    csdvdwd

--- Ryberries/Model/Goods.cs
+++ Ryberries/Model/Goods.cs
@@ -8,5 +8,21 @@ namespace Ryberries.Model
 {
     internal class Goods
     {
+        public int Article;
+        public string Name;
+        public int DealerArticle;
+
+        public int Cost;
+        public float Rating;
+        
+
+        public Goods(int article, string name, int dealerArticle, int cost, float rating)
+        {
+            Article = article;
+            Name = name;
+            DealerArticle = dealerArticle;
+            Cost = cost;
+            Rating = rating;
+        }
     }
 }
